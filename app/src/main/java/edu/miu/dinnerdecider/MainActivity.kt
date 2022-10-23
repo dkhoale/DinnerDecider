20822bfdf2d6b30edbbafd5981b63a9563711590
ldkhoa83@gmail.com <ldkhoa83@gmail.com>
2022-10-23 14:06:17 -0500

    Implement Task A and B

--- app/src/main/java/edu/miu/dinnerdecider/MainActivity.kt
+++ app/src/main/java/edu/miu/dinnerdecider/MainActivity.kt
@@ -2,10 +2,24 @@ package edu.miu.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val foods = arrayListOf("Hamburger","Pizza", "American", "Mexican", "Chinese")
+
+        decideBtn.setOnClickListener {
+            var pickedIndex: Int = (Math.random() * 100).toInt() % foods.size
+            tv.text = foods[pickedIndex]
+        }
+
+        addFoodBtn.setOnClickListener {
+            if(inputFoodText.text.isNotBlank()){
+                foods.add(inputFoodText.text.toString())
+            }
+        }
     }
 }
